19ebfc06eed3253bca3a9225a7dcb28d1f944499
Julián Capdevila <Julicapdevila30@gmail.com>
2026-09-24 15:04:00 -0300

    Clase Alumno

---
+++ Sistema de alumnos/Alumno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sistema_de_alumnos
+{
+    internal class Alumno
+    {
+        public string nombre {  get; set; }
+        public int legajo { get; set; }
+        public double nota1 { get; set; }
+        public double nota2 { get; set; }
+    }
+}
---
+++ Sistema de alumnos/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Sistema_de_alumnos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno();
+
+            alumno1.nombre = "Julián";
+            alumno1.legajo = 12345;
+            alumno1.nota1 = 8.00;
+            alumno1.nota2 = 10.00;
+
+            Alumno alumno2 = new Alumno();
+            alumno2.nombre = "Pablo";
+            alumno2.legajo = 678910;
+            alumno2.nota1 = 7.50;
+            alumno2.nota2 = 8.75;
+
+
+            Console.WriteLine($"Nombre: {alumno1.nombre}, Legajo = {alumno1.legajo}");
+            Console.WriteLine($"Nombre: {alumno2.nombre}, Legajo = {alumno2.legajo}");
+
+
+        }
+    }
+}
